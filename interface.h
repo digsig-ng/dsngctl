85512df3ff2aea46e0ee755b4385281cb0a2c1fc
Rupert Muchembled <rupert@rmuch.net>
2014-01-12 10:24:15 +0000

    Document functions in interface.h
    Add Doxygen documentation strings to interface.h.
    
    Signed-off-by: Rupert Muchembled <rupert@rmuch.net>

--- interface.h
+++ interface.h
@@ -12,10 +12,30 @@
 #ifndef DSNG_INTERFACE_H
 #define DSNG_INTERFACE_H
 
+/**
+ * Reads a public key from the provided path and loads it into the kernel
+ * module.
+ *
+ * @param pkey_path Path to public key.
+ *
+ * @returns 0 upon success, or negative integer representing error code.
+ */
 int dsng_start(char *pkey_path);
 
+/**
+ * Determines whether the kernel module has been loaded by attempting to stat
+ * a module sysfs file.
+ *
+ * @returns 1 if the module has been loaded, 0 otherwise.
+ */
 int digsig_is_loaded();
 
+/**
+ * Determines whether the kernel module is initialized by reading the module
+ * status from /sys/digsig/status.
+ *
+ * @returns 1 if the module has been initialized, 0 otherwise.
+ */
 int digsig_is_initialized();
 
 #endif /* DSNG_INTERFACE_H */
