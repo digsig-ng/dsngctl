3e4eec41ce14e79f3761c31fa15475a41bcf83c3
Rupert Muchembled <rupert@rmuch.net>
2014-01-14 20:29:57 +0000

    Update copyright year in interface.c/h
    Update the copyright year in interface.c and interface.h.
    
    Signed-off-by: Rupert Muchembled <rupert@rmuch.net>

--- interface.h
+++ interface.h
@@ -1,7 +1,7 @@
 /*
  * dsngctl - Control utility for DigSig-ng (digsig-ng.org)
  *
- * Copyright (c) 2013, The DigSig-ng Authors
+ * Copyright (c) 2013-2014, The DigSig-ng Authors
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
