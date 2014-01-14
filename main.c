a868bc6d36955d094c68c82e591ea72b293ca8d5
Rupert Muchembled <rupert@rmuch.net>
2014-01-14 20:40:46 +0000

    Update copyright year in main.c
    Update the copyright year in main.c.
    
    Signed-off-by: Rupert Muchembled <rupert@rmuch.net>

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
  *
  * main.c - command line utility
  *
- * Copyright (c) 2013, The DigSig-ng Authors
+ * Copyright (c) 2013-2014, The DigSig-ng Authors
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
